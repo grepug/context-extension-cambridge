b656eff2be36481d67e9ba025bbcc4c4023577fc
Kai Shao <grepug@icloud.com>
2023-05-25 15:26:22 +0800

    增加 look 的短语动词和习语的测试

--- test/look.test.ts
+++ test/look.test.ts
@@ -53,4 +53,104 @@ describe("test look", () => {
         "Look! There's grandma.",
       );
   });
+
+  // 测试短语动词
+  test("phrasal verbs", () => {
+    expect(data.definitionGroups[0].phrasalVerbs.length).toEqual(23);
+    expect(data.definitionGroups[0].phrasalVerbs[0].text).toEqual(
+      "look after sb/sth",
+    );
+
+    // 测试列表前5个
+    expect(
+      data.definitionGroups[0].phrasalVerbs.map((el: any) => el.text).slice(
+        0,
+        5,
+      ),
+    )
+      .toEqual(
+        [
+          "look after sb/sth",
+          "look ahead",
+          "look around (somewhere/sth)",
+          "look at sth",
+          "look back",
+        ],
+      );
+
+    // 测试列表后5个
+    expect(
+      data.definitionGroups[0].phrasalVerbs.map((el: any) => el.text).slice(-5),
+    )
+      .toEqual(
+        [
+          "look to sb for sth",
+          "look up",
+          "look sth up",
+          "look sb up",
+          "look up to sb",
+        ],
+      );
+    /**
+     * Expected: "phrasal verb"
+     * Received: "phrasal verbverb"
+     */
+    // expect(
+    //   data.definitionGroups[0].phrasalVerbs[0].definitionGroups[0].partOfSpeech,
+    // ).toEqual(
+    //   "phrasal verb",
+    // );
+
+    /**
+     *    Expected: "to take care of or be in charge of someone or something"
+          Received: ""
+     */
+    // expect(
+    //   data.definitionGroups[0].phrasalVerbs[0].definitionGroups[0].senses[0]
+    //     .text.rawText,
+    // ).toEqual("to take care of or be in charge of someone or something");
+
+    // 这里没有爬到释义的翻译
+    // expect(
+    //   data.definitionGroups[0].phrasalVerbs[0].definitionGroups[0].senses[0]
+    //     .text.translation.rawText,
+    // ).toEqual("照看，照顾，看管");
+  });
+
+  test("idioms", () => {
+    expect(data.definitionGroups[0].idioms.length).toEqual(14);
+    expect(
+      data.definitionGroups[0].idioms.map((el: any) => el.text).slice(-5),
+    ).toEqual([
+      "look straight/right through sb",
+      "look to your laurels",
+      "make sb look small",
+      "never look a gift horse in the mouth",
+      "never look back",
+    ]);
+
+    /**
+     * Expected: "idiom"
+       Received: ""
+     */
+    // expect(data.definitionGroups[0].idioms[0].definitionGroups[0].partOfSpeech)
+    //   .toEqual("idiom");
+
+    /**
+     *  Expected: "to be acting in a way that will certainly cause problems for you"
+        Received: ""
+     */
+    // expect(
+    //   data.definitionGroups[0].idioms[0].definitionGroups[0].senses[0].text
+    //     .rawText,
+    // ).toEqual(
+    //   "to be acting in a way that will certainly cause problems for you",
+    // );
+
+    // 这里没有爬到释义的翻译
+    // expect(
+    //   data.definitionGroups[0].idioms[0].definitionGroups[0].senses[0]
+    //     .text.translation.rawText,
+    // ).toEqual("招惹麻烦，自找麻烦");
+  });
 });
